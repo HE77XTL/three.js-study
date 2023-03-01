bce96b39ed628e54b31614cefc3d0c4a4bdbc64b
zoro <he77505357@163.com>
2023-03-01 10:32:23 +0800

    小修改

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import * as THREE from 'three';
-const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+import {onMounted} from 'vue';
+
+onMounted(()=>{
+  const camera = new THREE.PerspectiveCamera( 70, 1, 0.01, 10 );
 camera.position.z = 1;
 const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
@@ -10,20 +13,27 @@ const mesh = new THREE.Mesh( geometry, material );
 scene.add( mesh );
 
 const renderer = new THREE.WebGLRenderer( { antialias: true } );
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( 700, 700 );
 renderer.setAnimationLoop( animation );
-
-document.body.appendChild( renderer.domElement );
 
 function animation( time: number ) {
   mesh.rotation.x = time / 2000;
   mesh.rotation.y = time / 1000;
   renderer.render( scene, camera );  
 }
+
+const threeDemoBox =  document.getElementById('three-demo-1');
+if(threeDemoBox){
+  threeDemoBox.appendChild( renderer.domElement );
+}
+})
+
+
+
 </script>
 
 <template>
-  <div>
+  <div id="three-demo-1">
   </div>
 </template>
 
